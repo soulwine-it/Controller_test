2f02cdbf7863170f9caee3fddf702561e7d3b055
soulwine-it <skyorwind@gmail.com>
2020-01-08 21:51:49 +0900

    MultiActionController

---
+++ pro21/src/com/spring/ex02/UserController.java
@@ -0,0 +1,43 @@
+package com.spring.ex02;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+
+// 설정 파일의 userMethodNameResolver 프로퍼티를 사용하려면 반드시 MultiActionController를 상속 받아야 합니다.
+public class UserController extends MultiActionController{
+	public ModelAndView login(HttpServletRequest request, HttpServletResponse response) 
+			throws Exception{
+			String userID ="";
+			String passwd ="";
+			ModelAndView mav = new ModelAndView();
+			request.setCharacterEncoding("utf-8");
+			userID=request.getParameter("userID");
+			passwd=request.getParameter("passwd");
+			
+			//ModelAndView에 로그인 정보를 바인딩합니다.
+			mav.addObject("userID", userID);
+			mav.addObject("passwd", passwd);
+			//ModelAndView 객체에 포워딩할 JSP 이름을 설정합니다.
+			mav.setViewName("result");
+			return mav;
+		}
+	public ModelAndView memberInfo(HttpServletRequest request, HttpServletResponse response) throws Exception{
+		request.setCharacterEncoding("utf-8");
+		ModelAndView mav = new ModelAndView();
+		String id = request.getParameter("id");
+		String pwd= request.getParameter("pwd");
+		String name= request.getParameter("name");
+		String email=request.getParameter("email");
+		
+		mav.addObject("id", id);
+		mav.addObject("pwd", pwd);
+		mav.addObject("name", name);
+		mav.addObject("email", email);
+		
+		mav.setViewName("memberInfo");
+		return mav;
+	}
+}
